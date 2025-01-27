a91c882cd4cb0e725f5ba7cda4661ba90a8b0c9f
Raphazelll <pimentaraphael000@gmail.com>
2025-01-27 19:42:07 -0300

    conceito de árvora

---
+++ src/anotacoes.java
@@ -0,0 +1,41 @@
+public class anotacoes {
+
+    /*
+     * Conceito de Árvores
+     * - Estrutura de dados bidimensional
+     * - Não linear
+     * - Constituída por nós que representam um modelo hierárquico
+     * Armazenam os dados com base em relações de dependências.
+     * - Listas, Filas e Pilhas são exemplos de estruturas lineares.
+     * 
+     * Utilização:
+     * - Sistemas de arquivos
+     * - banco de dados
+     * - interfaces gráficas
+     * - páginas web
+     * 
+     * 
+     * Representações:
+     * 
+     * Hierárquica
+     * - Cada nó pode ter vários filhos, mas apenas um pai.
+     * - Exemplo: Árvore Genealógica
+     * Por inclusão
+     * - Cada nó pode ter vários pais, mas apenas um filho.
+     * - Exemplo: Sistema de arquivos
+     * Por parênteses Aninhados
+     * - Cada nó pode ter vários filhos e vários pais.
+     * 
+     * Características de uma Árvora Hierárquica
+     * Nó
+     * Raiz
+     * Pai e filho
+     * Irmão
+     * Nível de um nó (Posição hierárquica com relação a raiz.)
+     * Altura ou profundidade (grau máximo dos nós)
+     * Folha ou nó terminal
+     * Nó interno
+     * Grau de um nó
+     * Subárvora
+     */
+}
