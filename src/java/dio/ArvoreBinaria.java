c49eea325ef8ac00f12df05f058544ee2ba60728
Raphazelll <pimentaraphael000@gmail.com>
2025-01-29 15:40:33 -0300

    atualização aula árvore

--- src/java/dio/ArvoreBinaria.java
+++ src/java/dio/ArvoreBinaria.java
@@ -1,27 +1,28 @@
 package java.dio;
 
-public class ArvoreBinaria<T> extends Comparable<T>> {
+public class ArvoreBinaria<T> extends Comparable<T>> { // Classe Ar
 
-    private BinNo<T> raiz;
+    private BinNo<T> raiz; // A raiz da árvore
 
-    public ArvoreBinaria() {
+    public ArvoreBinaria() { // Construtor
         this.raiz = null;
     }
 
-    public void inserir(T conteudo) {
-        BinNo<T> novoNo = new BinNo<>(conteudo);
+    public void inserir(T conteudo) { // Método de inserção
+        BinNo<T> novoNo = new BinNo<>(conteudo); // Cria um novo nó
         raiz = inserir(raiz, novoNo);
     }
 
     private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) { // Método de inserção
-        if (atual == null) {
-            return novoNo;
+        if (atual == null) { // Se a raiz estiver vazia, insere o novo nó
+            return novoNo; // Retorna o novo nó
         } else {
             if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
-                atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+                atual.setNoEsq(inserir(atual.getNoEsq(), novoNo)); // Se o novo nó for menor que o atual, vai para a esquerda
             } else {
-                atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+                atual.setNoDir(inserir(atual.getNoDir(), novoNo)); // Se o novo nó for maior que o atual, vai para a direita
             }
-            return atual;
+            return atual; // Retorna o nó atual
         }
     }
+}
